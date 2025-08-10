34ae0b21cea0cb20d1b7111cf1ab09bca2bc3f97
SDCore <SDCore@users.noreply.github.com>
2025-08-09 21:10:50 -0500

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,1 +1,13 @@
-Welcome, Pilot.
+<!DOCTYPE html>
+
+<html>
+  <head>
+    <title>Welcome, Pilot.</title>
+  </head>
+
+  <body>
+    Your skills will be useful.
+    <br />
+    <?php echo date("Y"); ?>
+  </body>
+</html>
